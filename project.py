2a9c8177ed9a213f17b666b67cf2327f1cf797b1
Mohammad Mustafa <37963449+MoMustafa@users.noreply.github.com>
2018-09-30 17:31:16 -0500

    v1.0

--- project.py
+++ project.py
@@ -5,112 +5,115 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib
-from urllib.request import Request, urlopen
+from urllib.request import Request
 import re
 import os
-import errno
 import nltk
-import string
 from nltk import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
-from unicodedata import normalize
+
 import time
 
 start_time = time.time()
 
 parser = 'lxml'
-header = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'User-Agent': 'Mozilla/5.0'}      
+header = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'User-Agent': 'Mozilla/5.0'} 
+omit = ['contact','advertise','privacy','policy','terms','writers','#','dmca', 'forum','news','author','our-team']
+urlfile = 'urls.txt'
+starter_url = "http://www.thatvideogameblog.com/news/page/"
+pages = 8
+important = 1
 
-def compile_urls(fname, webpage):
-    print('in compile')
-    omit = ['contact','advertise','privacy','policy','terms','writers','#',
-            'dmca', 'forum','news','author','our-team']
-    with open(fname, 'a+') as f:
+def compile_urls(webpage):
+    with open(urlfile, 'a+') as f:
         for link in webpage.findAll('a'):
             url = str(link.get('href'))
             if not any(word in url for word in omit) and url.startswith('http'):
-                    f.write(url+'\n')          
+                f.write(url+'\n') 
 
-def update_urls():
-    print('in update')
-    with open('urls.txt') as f:
-        lines = list(set(f.read().split('\n')))
-        if '' in lines:
-            lines.remove('')
-        for url in lines:
-            req = Request(url, headers=header)
+def clean_urls():
+    sites = []
+    with open(urlfile, 'r') as f:
+        for line in f:
+            sites.append(line)
+    sites = list(set(sites))
+    with open('urls.txt', 'w') as f:
+        for line in sites:
+            f.write(line)
+
+def read_urls():
+    with open(urlfile, 'r') as f:
+        for line in f:
+            print("--- %s seconds ---" % (time.time() - start_time))
+            req = Request(line, headers=header)
             html = urllib.request.urlopen(req)
-            page = BeautifulSoup(html, parser)
-            data = page.findAll('p')
-            if any(word in page.title.string for word in ['Archives']):
-                compile_urls('urls.txt', page)        
-                 
-def read_url():
-    print('in read')
-    with open('urls.txt') as f:
-        lines = list(set(f.read().split('\n')))
-        if '' in lines:
-            lines.remove('')
-    for url in lines:
-        req = Request(url, headers=header)
-        html = urllib.request.urlopen(req)
-        soup = BeautifulSoup(html, parser)
-        data = soup.findAll('p')
-        omit = ['id','style']
-        if not any(word in soup.title.string for word in ['Archives']):
-            title ='Webpages\\' + re.sub('[/*\.\\\[,:;\]\|=?<>|]|\s{2,}'," ",soup.title.string)+'.txt'
-            os.makedirs(os.path.dirname(title), exist_ok=True)
+            soup = BeautifulSoup(html, parser)
+            data = soup.findAll('p')
+            omit = ['id','style']
+            if not any(word in soup.title.string for word in ['Archives']):
+                title ='Webpages\\' + re.sub('[^\w]+|\s{2,}'," ",soup.title.string)+'.txt'
+                os.makedirs(os.path.dirname(title), exist_ok=True)
             with open(title, 'wb') as f:
                 for x in data:
                     if not any(words in omit for words in x.parent.attrs):
-                        f.write(x.get_text().encode('utf-8'))                 
-                        
-def cleanup_text():
-    print('in cleanup')
-    omit = stopwords.words('english')+list(string.punctuation)
+                        f.write(x.get_text().encode('utf-8')) 
+            if os.stat(title).st_size == 0:
+                os.remove(title)
+
+def clean_text():
     for file in os.listdir():
-        with open(file, 'r', encoding='utf-8') as f:
-            text = f.read()
-            for x in [".",',',':']:
-                    text = text.replace(x, ' ')
-        sentences = sent_tokenize(text)
-        with open('CLEANED '+file, 'w', encoding='utf-8') as f:
+        with open(file, 'r', encoding='utf-8') as f, open('CLEANED '+file, 'w', encoding='utf-8') as c:
+            text = f.read().lower()
+            sentences = sent_tokenize(text)
             for sent in sentences:
-                sent = sent.lower()
-                words = word_tokenize(sent)
-                for word in words:
-                    if word.isalpha() and not word in omit:
-                        f.write(word +' ')
-            
-def extractor():
-    print('in extractor')
-    for file in os.listdir():
-        if file.startswith('CLEANED '):
-            with open(file, 'r', encoding='utf-8') as f:
-                text = f.read()
-                words = word_tokenize(text)
-            print('\n'+file[8:])    
-            print(nltk.FreqDist(words).most_common(15))
+                sent = re.sub('[\W]+|(\w*)\d+(.)',' ', sent)
+                sent = re.sub('\s{1,}',' ', sent)
+                c.write(sent+'\n')
                 
+def extractor():
+    omit = stopwords.words('english')
+    d = {}
+    for file in os.scandir():
+        if file.name.startswith('CLEANED'):
+            words = []
+            with open(file, 'r', encoding='utf-8') as f:
+                for line in f:
+                    for word in word_tokenize(line):
+                        if word not in omit:
+                            words.append(word)
+            common = important * len(nltk.FreqDist(words))
+            dist = nltk.FreqDist(words).most_common(int(common))
+            for k,v in dist:
+                d[k] = v+d[k] if k in d else v
+    return d
+
+def demo(vocab):
+    top = sorted(vocab.items(), key = lambda x: x[1], reverse=True)
+    print('Top 40 Dictionary Terms')
+    for i in range(0,80):
+            print("%-15s: %i" %(top[i][0],top[i][1]))
+    
+    terms=['game','gaming','experience','series','available','story', 'play', 'new', 'character','display'] 
+    print('\nManually selected Top 10 Terms')
+    for term in terms:
+        print(term)
+        
 def main():
-    print('in main')
-    starter_url = "http://www.thatvideogameblog.com/news/page/"
-    for i in range(1,11):
+    for i in range(1,pages):
         url = starter_url + str(i)
         r = requests.get(url)
         data = r.text
         page = BeautifulSoup(data, parser)
-        compile_urls('urls.txt', page)
-    print("\n--- %s seconds ---" % (time.time() - start_time))
-    #update_urls()
-    print("\n--- %s seconds ---" % (time.time() - start_time))
-    read_url()
-    print("\n--- %s seconds ---" % (time.time() - start_time))
+        compile_urls(page)
+    clean_urls()
+    read_urls()
     os.chdir('Webpages')
-    cleanup_text()
-    print("\n--- %s seconds ---" % (time.time() - start_time))
-    extractor()
-    print("\n--- %s seconds ---" % (time.time() - start_time))
-               
+    clean_text()
+    vocab = extractor()
+    demo(vocab)
+    
+    
+    
+    
 if __name__ == '__main__':    
-    main()
+    main()    
