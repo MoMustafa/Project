e4fb7e659bd2d89bdb324b6bcfe096b69fc67689
Mohammad Mustafa <37963449+MoMustafa@users.noreply.github.com>
2018-09-30 23:21:52 -0500

    v1.1
    Added documentation

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# NLP_Chatbot
+Project - NLP Chatbot Phase I
+Mohammad Mustafa
+msm151030
+
+DOCUMENTATION
+
+main()
+    Uses the Beautiful Soup library to read several pages (determined by the
+    'pages' variable) starting from the assigned starter_url.
+    For each webpage, calles the compile_urls() function
+    Then clean_urls() is called to remove duplicate links.
+    Then read_urls() is called to go through the list of retrieved webpage links
+    and reads and documents them. 
+    Then the clean_text() method removes numerals, special characters, 
+    newlines etc. from the files and writes a new file.
+    The extractor() method is then called and returns a dictonary of the 
+    vocabulary extracted from the knowledge base of cleaned files.
+    
+compile_urls()
+    Opens/creates the designated file for storing retrieved urls and reads
+    through each html document and writes the urls contained within the 
+    anchor tags.
+    
+clean_urls()
+    reads the list of urls and appends the url to a list. By converting the
+    list to a set and then back to a list removes any duplicates and the 
+    new list is overwrritten on the .txt file of urls
+    
+read_urls()
+    Opens each url in the .txt file of urls. Reads the text contained within
+    the paragraph tags of the html document and a new .txt file is written 
+    for each webpage containing the text read from that webpage. Text is only
+    written to a new file if it does not fall under any of the omitted
+    catagories. If the new .txt file is empty, it is deleted.
+    
+clean_text()
+    Opens each of the files written by read_urls() reads the text and 
+    lowercases it. Then it tokenizes the lines using the sent_tokenize nltk 
+    method. Numerals, special characters, newlines and extra whitespaces are 
+    removed from the sentences using Regular Expressions. The cleaned up 
+    sentences are written to a new file with the 'CLEANED' suffix in its title  
+
+extractor()
+    For each 'CLEANED' file in the knowledge base directory, it reads each 
+    line and word_tokenizes it. If the word is not a stopword, it is appended 
+    to a list. After reading all the files, the FreqDist() nltk function 
+    generates a frequency distribution of all the words. 
+    What percentage of the common words are included in the distribution is
+    determined by the 'important' variable. 
+    A dictionary is created from the frequency distribution and returned.
+    
+demo()
+    takes the 'vocab' dictionary returned by the extractor() from main and
+    sorts it by word frequency. It then prints the top 40 words in the
+    dictionary to the console. Then it prints my handpicked list of 10 terms.
--- project.py
+++ project.py
@@ -1,5 +1,59 @@
 # =============================================================================
-# Mohammad Mustafa NLP Chatbot 
+# Project - NLP Chatbot Phase I
+# Mohammad Mustafa
+# msm151030
+# 
+# DOCUMENTATION
+# 
+# main()
+#     Uses the Beautiful Soup library to read several pages (determined by the
+#     'pages' variable) starting from the assigned starter_url.
+#     For each webpage, calles the compile_urls() function
+#     Then clean_urls() is called to remove duplicate links.
+#     Then read_urls() is called to go through the list of retrieved webpage links
+#     and reads and documents them. 
+#     Then the clean_text() method removes numerals, special characters, 
+#     newlines etc. from the files and writes a new file.
+#     The extractor() method is then called and returns a dictonary of the 
+#     vocabulary extracted from the knowledge base of cleaned files.
+#     
+# compile_urls()
+#     Opens/creates the designated file for storing retrieved urls and reads
+#     through each html document and writes the urls contained within the 
+#     anchor tags.
+#     
+# clean_urls()
+#     reads the list of urls and appends the url to a list. By converting the
+#     list to a set and then back to a list removes any duplicates and the 
+#     new list is overwrritten on the .txt file of urls
+#     
+# read_urls()
+#     Opens each url in the .txt file of urls. Reads the text contained within
+#     the paragraph tags of the html document and a new .txt file is written 
+#     for each webpage containing the text read from that webpage. Text is only
+#     written to a new file if it does not fall under any of the omitted
+#     catagories. If the new .txt file is empty, it is deleted.
+#     
+# clean_text()
+#     Opens each of the files written by read_urls() reads the text and 
+#     lowercases it. Then it tokenizes the lines using the sent_tokenize nltk 
+#     method. Numerals, special characters, newlines and extra whitespaces are 
+#     removed from the sentences using Regular Expressions. The cleaned up 
+#     sentences are written to a new file with the 'CLEANED' suffix in its title  
+# 
+# extractor()
+#     For each 'CLEANED' file in the knowledge base directory, it reads each 
+#     line and word_tokenizes it. If the word is not a stopword, it is appended 
+#     to a list. After reading all the files, the FreqDist() nltk function 
+#     generates a frequency distribution of all the words. 
+#     What percentage of the common words are included in the distribution is
+#     determined by the 'important' variable. 
+#     A dictionary is created from the frequency distribution and returned.
+#     
+# demo()
+#     takes the 'vocab' dictionary returned by the extractor() from main and
+#     sorts it by word frequency. It then prints the top 40 words in the
+#     dictionary to the console. Then it prints my handpicked list of 10 terms.
 # =============================================================================
 
 from bs4 import BeautifulSoup
@@ -12,17 +66,29 @@ import nltk
 from nltk import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 
-import time
-
-start_time = time.time()
-
 parser = 'lxml'
 header = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'User-Agent': 'Mozilla/5.0'} 
 omit = ['contact','advertise','privacy','policy','terms','writers','#','dmca', 'forum','news','author','our-team']
+
 urlfile = 'urls.txt'
 starter_url = "http://www.thatvideogameblog.com/news/page/"
-pages = 8
+
+pages = 2
 important = 1
+
+def main():
+    for i in range(1,pages):
+        url = starter_url + str(i)
+        r = requests.get(url)
+        data = r.text
+        page = BeautifulSoup(data, parser)
+        compile_urls(page)
+    clean_urls()
+    read_urls()
+    os.chdir('Webpages')
+    clean_text()
+    vocab = extractor()
+    demo(vocab)
 
 def compile_urls(webpage):
     with open(urlfile, 'a+') as f:
@@ -44,7 +110,6 @@ def clean_urls():
 def read_urls():
     with open(urlfile, 'r') as f:
         for line in f:
-            print("--- %s seconds ---" % (time.time() - start_time))
             req = Request(line, headers=header)
             html = urllib.request.urlopen(req)
             soup = BeautifulSoup(html, parser)
@@ -83,37 +148,20 @@ def extractor():
                             words.append(word)
             common = important * len(nltk.FreqDist(words))
             dist = nltk.FreqDist(words).most_common(int(common))
-            for k,v in dist:
-                d[k] = v+d[k] if k in d else v
+            for key,value in dist:
+                d[key] = value+d[key] if key in d else value
     return d
 
 def demo(vocab):
     top = sorted(vocab.items(), key = lambda x: x[1], reverse=True)
     print('Top 40 Dictionary Terms')
-    for i in range(0,80):
+    for i in range(0,40):
             print("%-15s: %i" %(top[i][0],top[i][1]))
     
     terms=['game','gaming','experience','series','available','story', 'play', 'new', 'character','display'] 
     print('\nManually selected Top 10 Terms')
     for term in terms:
         print(term)
-        
-def main():
-    for i in range(1,pages):
-        url = starter_url + str(i)
-        r = requests.get(url)
-        data = r.text
-        page = BeautifulSoup(data, parser)
-        compile_urls(page)
-    clean_urls()
-    read_urls()
-    os.chdir('Webpages')
-    clean_text()
-    vocab = extractor()
-    demo(vocab)
-    
-    
-    
     
 if __name__ == '__main__':    
     main()    
